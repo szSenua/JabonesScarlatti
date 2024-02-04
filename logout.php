d50a3452679f188bad8e4d203eeab3ea270297ff
Senua <chc0089@gmail.com>
2024-02-04 18:16:29 +0100

    Fixed sessions

--- logout.php
+++ logout.php
@@ -1,10 +1,18 @@
 <?php
 
-//leo la sesión
 session_start();
 
-//Destruyo la sesión y redirigo al login
+//Eliminar completamente la sesión
+session_unset();
 session_destroy();
+session_write_close();
+
+//Barrido de cookie
+setcookie(session_name(), '', 0, '/');
+
+//Evita que se reutilice
+session_regenerate_id(true);
+
 
 header('Location: menu.php');
 
